9a6876107b288650dc1aa8259f04b318bcbae159
ZiqiCode <123768494+ZiqiCode@users.noreply.github.com>
2023-02-09 12:58:39 -0500

    Update lc4_cla.v

--- lab2b-alu/lc4_cla.v
+++ lab2b-alu/lc4_cla.v
@@ -28,7 +28,12 @@ module gp4(input wire [3:0] gin, pin,
            input wire cin,
            output wire gout, pout,
            output wire [2:0] cout);
-   
+  assign gout = gin[0] & gin[1] & gin[2] & gin[3];
+  assign pout = pin[0] | pin[1] | pin[2] | pin[3];
+  assign cout[0] = gin[0] & pin[0] | gin[0] & cin | pin[0] & cin;
+  assign cout[1] = gin[1] & pin[1] | gin[1] & cout[0] | pin[1] & cout[0];
+  assign cout[2] = gin[2] & pin[2] | gin[2] & cout[1] | pin[2] & cout[1];
+
 endmodule
 
 /**
